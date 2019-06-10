dba03d16c5ca341dc4bb03bdc551a244dc0b7784
unknown <elkana911@yahoo.com>
2019-06-10 15:13:03 +0700

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilesUploadComponent } from './files-upload/files-upload.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/first', pathMatch: 'full' },
+  { path: 'first', component: FilesUploadComponent},
+  // { path: 'second', component: Tab2Component},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilesUploadComponent } from './files-upload/files-upload.component';
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule, MatInputModule, MatSelectModule, MatIconModule } from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilesUploadComponent,
+    FileSelectDirective,FileDropDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
